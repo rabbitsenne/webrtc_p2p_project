68498210a9c005a5423389d0e784a6bbeb67db3b
rabbit <rego11senne@gmail.com>
2023-05-18 15:33:25 +0200

    I have created a PeerConnection and declared an offer to send to my peer

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-let localStream; //my camera and audio
-let remoteStream; //the other users camera and audio
+let localStream; //local computer. camera and audio
+let remoteStream; //remote peer. camera and audio
+let peerConnection;
 
 let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false}) //asking for user permissions
     document.getElementById('user-1').srcObject = localStream;
+
+    createOffer()
 }
 
+let createOffer = async() => {
+    peerConnection = new RTCPeerConnection(); // we have created a peerConnection between the local and remote machine
+    
+    remoteStream = new MediaStream();
+    document.getElementById('user-2').srcObject = remoteStream;
+    
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    console.log('Offer:',offer)
+}
 init()
